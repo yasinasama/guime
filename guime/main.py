2844a70e443d77042dea5b3785f3530528f642f3
614867000@qq.com <614867000@qq.com>
2019-03-10 22:22:50 +0800

    update

--- guime/main.py
+++ guime/main.py
@@ -4,83 +4,111 @@ import wx
 class UI(wx.Frame):
 
     def __init__(self,):
-        super(UI, self).__init__(None,style=wx.DEFAULT_FRAME_STYLE | wx.MAXIMIZE)
+        super(UI, self).__init__(None,style=wx.MAXIMIZE |wx.DEFAULT_FRAME_STYLE)
 
         self.window_size = (1000,600)
         self.cell_size = (60,42)
 
-        self.__init()
+        self.tcs = {}
+        self.tc_names = [
+            'order_id','car_frame',
+            'order_time','car_id',
+            'car_type','car_owner','phone'
+        ]
 
+        self.__init()
+        
     def __init(self):
         self.panel = wx.Panel(self)
         # 菜单栏
-        menubar = wx.MenuBar()
-        fix_order = wx.Menu()
-        menubar.Append(fix_order, '&维修工单')
-        self.SetMenuBar(menubar)
+        # menubar = wx.MenuBar()
+        # fix_order = wx.Menu()
+        # menubar.Append(fix_order, '&维修工单')
+        # self.SetMenuBar(menubar)
 
         # 工具栏
-        self.toolbar = self.CreateToolBar()
+        # self.toolbar = self.CreateToolBar()
         # print(self.toolbar.GetMargins())
         # self.toolbar.SetMargins(100,200)
         # print(self.toolbar.GetMargins())
 
-        fix_order_new = self.toolbar.AddTool(wx.ID_ANY, 'Quit', wx.Bitmap('../icons/fix_order_new.png'),'新建')
-        fix_order_save = self.toolbar.AddTool(wx.ID_ANY, 'Quit', wx.Bitmap('../icons/fix_order_save.png'), '保存')
-        fix_order_delete = self.toolbar.AddTool(wx.ID_ANY, 'Quit', wx.Bitmap('../icons/fix_order_delete.png'), '删除')
-        self.toolbar.AddSeparator()
-        fix_order_delete2 = self.toolbar.AddTool(wx.ID_ANY, 'Quit', wx.Bitmap('../icons/fix_order_delete.png'))
+        # fix_order_new = self.toolbar.AddTool(wx.ID_ANY, 'Quit', wx.Bitmap('../icons/fix_order_new.png'),'新建')
+        # fix_order_save = self.toolbar.AddTool(wx.ID_ANY, 'Quit', wx.Bitmap('../icons/fix_order_save.png'), '保存')
+        # fix_order_delete = self.toolbar.AddTool(wx.ID_ANY, 'Quit', wx.Bitmap('../icons/fix_order_delete.png'), '删除')
+        # self.toolbar.AddSeparator()
+        # fix_order_delete2 = self.toolbar.AddTool(wx.ID_ANY, 'Quit', wx.Bitmap('../icons/fix_order_delete.png'))
 
-        self.toolbar.Realize()
-        self.Bind(wx.EVT_TOOL, self.ShowMessage, fix_order_new)
-        self.Bind(wx.EVT_TOOL, self.close, fix_order_save)
-        self.Bind(wx.EVT_TOOL, self.close, fix_order_delete)
-        self.Bind(wx.EVT_TOOL, self.close, fix_order_delete2)
+        # self.toolbar.Realize()
+        # self.Bind(wx.EVT_TOOL, self.ShowMessage, fix_order_new)
+        # self.Bind(wx.EVT_TOOL, self.close, fix_order_save)
+        # self.Bind(wx.EVT_TOOL, self.close, fix_order_delete)
+        # self.Bind(wx.EVT_TOOL, self.close, fix_order_delete2)
 
         # 布局
         main_box = wx.GridBagSizer()
-        main_box.SetEmptyCellSize((20, 20))
+        main_box.SetEmptyCellSize((110, 20))
+
+        new_btn = wx.Button(self.panel, -1, "新建")
+        main_box.Add(new_btn,pos=(0,0), flag=wx.ALL,border=5)
+
+        search_btn = wx.Button(self.panel, -1, "查询")
+        main_box.Add(search_btn,pos=(0,1), flag=wx.ALL,border=5)
+
+        search_btn.Bind(wx.EVT_BUTTON,self.search_btn_click)
+        
         st_order_id = wx.StaticText(self.panel, label="结算单号: ",style=wx.ALIGN_LEFT)
-        main_box.Add(st_order_id,pos=(0,0), flag=wx.TOP|wx.BOTTOM|wx.LEFT,border=10)
+        main_box.Add(st_order_id,pos=(1,0), flag=wx.TOP|wx.BOTTOM|wx.LEFT,border=10)
         tc_order_id = wx.TextCtrl(self.panel)
-        main_box.Add(tc_order_id,pos=(0,1), span=(1,2), flag=wx.EXPAND|wx.TOP|wx.BOTTOM,border=10)
+        main_box.Add(tc_order_id,pos=(1,1), span=(1,2), flag=wx.EXPAND|wx.TOP|wx.BOTTOM,border=10)
+        self.tcs['order_id'] = tc_order_id
 
         st_car_frame = wx.StaticText(self.panel, label="车架号: ", style=wx.ALIGN_LEFT)
-        main_box.Add(st_car_frame, pos=(0, 3), flag=wx.TOP | wx.BOTTOM | wx.LEFT, border=10)
+        main_box.Add(st_car_frame, pos=(1, 3), flag=wx.TOP | wx.BOTTOM | wx.LEFT, border=10)
         tc_car_frame = wx.TextCtrl(self.panel)
-        main_box.Add(tc_car_frame, pos=(0, 4), span=(1, 3), flag=wx.EXPAND|wx.TOP | wx.BOTTOM, border=10)
+        main_box.Add(tc_car_frame, pos=(1, 4), span=(1, 3), flag=wx.EXPAND|wx.TOP | wx.BOTTOM, border=10)
+        self.tcs['car_frame'] = tc_car_frame
 
         st_order_time = wx.StaticText(self.panel, label="开单时间: ", style=wx.ALIGN_LEFT)
-        main_box.Add(st_order_time, pos=(0,7), flag=wx.TOP|wx.BOTTOM|wx.LEFT,border=10)
+        main_box.Add(st_order_time, pos=(1,7), flag=wx.TOP|wx.BOTTOM|wx.LEFT,border=10)
         tc_order_time = wx.TextCtrl(self.panel)
-        main_box.Add(tc_order_time, pos=(0,8), span=(1,4), flag=wx.EXPAND|wx.TOP|wx.BOTTOM,border=10)
+        main_box.Add(tc_order_time, pos=(1,8), span=(1,3), flag=wx.EXPAND|wx.TOP|wx.BOTTOM,border=10)
+        self.tcs['order_time'] = tc_order_time
 
         st_car_id = wx.StaticText(self.panel, label="车牌号: ", style=wx.ALIGN_LEFT)
-        main_box.Add(st_car_id, pos=(1,0), flag=wx.TOP|wx.BOTTOM|wx.LEFT,border=10)
+        main_box.Add(st_car_id, pos=(2,0), flag=wx.TOP|wx.BOTTOM|wx.LEFT,border=10)
         tc_car_id = wx.TextCtrl(self.panel)
-        main_box.Add(tc_car_id, pos=(1,1), span=(1,2), flag=wx.EXPAND|wx.TOP|wx.BOTTOM,border=10)
+        main_box.Add(tc_car_id, pos=(2,1), span=(1,2), flag=wx.EXPAND|wx.TOP|wx.BOTTOM,border=10)
+        self.tcs['car_id'] = tc_car_id
 
         st_car_type = wx.StaticText(self.panel, label="车型: ", style=wx.ALIGN_LEFT)
-        main_box.Add(st_car_type, pos=(1,3), flag=wx.TOP|wx.BOTTOM|wx.LEFT,border=10)
+        main_box.Add(st_car_type, pos=(2,3), flag=wx.TOP|wx.BOTTOM|wx.LEFT,border=10)
         tc_car_type = wx.TextCtrl(self.panel)
-        main_box.Add(tc_car_type, pos=(1,4), span=(1,2), flag=wx.EXPAND|wx.TOP|wx.BOTTOM,border=10)
+        main_box.Add(tc_car_type, pos=(2,4), span=(1,2), flag=wx.EXPAND|wx.TOP|wx.BOTTOM,border=10)
+        self.tcs['car_type'] = tc_car_type
+
+
 
         st_car_owner = wx.StaticText(self.panel, label="车主: ", style=wx.ALIGN_LEFT)
-        main_box.Add(st_car_owner, pos=(2, 0), flag=wx.TOP | wx.BOTTOM | wx.LEFT, border=10)
+        main_box.Add(st_car_owner, pos=(3, 0), flag=wx.TOP | wx.BOTTOM | wx.LEFT, border=10)
         tc_car_owner = wx.TextCtrl(self.panel)
-        main_box.Add(tc_car_owner, pos=(2, 1), span=(1, 2), flag=wx.EXPAND|wx.TOP | wx.BOTTOM, border=10)
+        main_box.Add(tc_car_owner, pos=(3, 1), span=(1, 2), flag=wx.EXPAND|wx.TOP | wx.BOTTOM, border=10)
+        self.tcs['car_owner'] = tc_car_owner
+
 
         st_phone = wx.StaticText(self.panel, label="联系电话: ", style=wx.ALIGN_LEFT, )
-        main_box.Add(st_phone, pos=(2, 3), flag=wx.TOP | wx.BOTTOM | wx.LEFT, border=10)
+        main_box.Add(st_phone, pos=(3, 3), flag=wx.TOP | wx.BOTTOM | wx.LEFT, border=10)
         tc_phone = wx.TextCtrl(self.panel)
-        main_box.Add(tc_phone, pos=(2, 4), span=(1, 2), flag=wx.EXPAND|wx.TOP | wx.BOTTOM, border=10)
+        main_box.Add(tc_phone, pos=(3, 4), span=(1, 2), flag=wx.EXPAND|wx.TOP | wx.BOTTOM, border=10)
+        self.tcs['phone'] = tc_phone
+
 
         st_remark = wx.StaticText(self.panel, label="备注: ", style=wx.ALIGN_LEFT)
-        main_box.Add(st_remark,pos=(1,7), flag=wx.ALL,border=10)
+        main_box.Add(st_remark,pos=(2,6), flag=wx.ALL,border=10)
         tc_remark = wx.TextCtrl(self.panel,style=wx.TE_MULTILINE)
-        main_box.Add(tc_remark,pos=(1,8), span=(2,4), flag=wx.EXPAND|wx.TOP|wx.BOTTOM,border=10)
+        main_box.Add(tc_remark,pos=(2,7), span=(2,4), flag=wx.EXPAND|wx.TOP|wx.BOTTOM,border=10)
 
-        print(main_box.FindItemAtPosition((1,8)))
+        
+
 
         self.lc = wx.ListCtrl(self.panel, 0, style=wx.LC_REPORT | wx.EXPAND)
         columns = [
@@ -97,9 +125,9 @@ class UI(wx.Frame):
         for index, column in enumerate(columns):
             self.lc.InsertColumn(index, column)
         self.Bind(wx.EVT_LIST_ITEM_ACTIVATED, self.select, self.lc)
-        main_box.Add(self.lc,pos=(4,0), span=(8,12), flag=wx.BOTTOM | wx.LEFT,border=10)
+        main_box.Add(self.lc,pos=(5,0), span=(24,15), flag=wx.EXPAND |wx.ALL,border=10)
         #
-        # self.lc2 = wx.ListCtrl(self.panel, 0, style=wx.LC_REPORT | wx.EXPAND, size=(400, 300))
+        # self.lc2 = wx.ListCtrl(self.panel, 0, style=wx.LC_REPORT | wx.EXPAND)
         # columns2 = [
         #     '维修单号',
         #     '维修项目',
@@ -109,18 +137,18 @@ class UI(wx.Frame):
         # for index, column in enumerate(columns2):
         #     self.lc2.InsertColumn(index, column)
         # self.Bind(wx.EVT_LIST_ITEM_ACTIVATED, self.select2, self.lc2)
-        # main_box.Add(self.lc2, pos=(4, 13), span=(4,4), flag=wx.EXPAND |wx.BOTTOM | wx.LEFT, border=10)
+        # main_box.Add(self.lc2, pos=(5, 11), span=(10,5), flag=wx.EXPAND |wx.BOTTOM | wx.LEFT, border=10)
 
         # # size
-        # main_box.AddGrowableRow(6)
+        main_box.AddGrowableCol(14)
         self.panel.SetSizerAndFit(main_box)
         self.SetSize(self.window_size)
-
-
         # 标题
         self.SetTitle('GUIME')
         # 居中
         self.Centre()
+
+        self.Maximize(True)
 
     def select2(self,e):
         choice = self.lc2.GetFirstSelected()
@@ -157,12 +185,19 @@ class UI(wx.Frame):
         wx.MessageBox('Download completed', 'Info',
                       wx.OK | wx.ICON_INFORMATION)
 
+    def search_btn_click(self,e):
+        for name,tc in self.tcs.items():
+            value = tc.GetValue()
+            if not value.strip():
+                continue
+            print(value)
+
 
 def main():
 
     app = wx.App()
     ex = UI()
-    # ex.set_values([['1','2','33333333111111111111122132134123413412341341sssssss','4','5','6','7','8','9']]*10)
+    ex.set_values([['1','2','33333333111111111111122132134123413412341341sssssss','4','5','6','7','8','9']]*20)
     ex.show()
     app.MainLoop()
 
